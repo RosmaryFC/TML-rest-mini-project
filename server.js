46d6bdfbd187f53a14e772a22eb67e08ca9c6d82
Rosmary Fermin <Rosmaryfc@hotmail.com>
2021-03-27 18:43:57 -0400

    created routes

--- server.js
+++ server.js
@@ -17,15 +17,90 @@ app.use(express.json())
 
 //ROUTES
 app.get("/", function(req, res) {
-    res.send("hello")
+    res.send("home page")
 });
 
 
+
 //BUSINESS ROUTERS
+//route: /business GET
+// shows all businesses created
+app.get("/business", function(req, res) {
+    res.send("show businesses")
+});
+
+//route: /business POST
+//create a business
+app.post("/business", function(req, res) {
+
+});
+
+//route: /business/id/:id GET
+//shows business with that id, and its posts
+app.get("/business/id/:id", function(req, res) {
+    res.send("show specific business")
+});
+
+//route: /business/id/:id PUT
+//will update business info
+app.put("/business/id/:id", function(req, res) {
+
+});
+
+//route: /business/id/:id DELETE
+//will delete business, posts, comments
+app.delete("/business/id/:id", function(req, res) {
+
+});
+
+
 
 //POST ROUTERS
+//route: /business/id/:id POST
+//create a post
+app.post("/business/id/:id", function(req, res) {
+
+});
+
+//route: /business/id/:id/post/:postid GET
+//get post info and comments for specific business
+app.get("/business/id/:id/post/:postid", function(req, res) {
+    res.send("show post")
+});
+
+//route: /business/id/:id/post/:postid PUT
+//update a post
+app.put("/business/id/:id/post/:postid", function(req, res) {
+
+});
+
+//route: /business/id/:id/post/:postid DELETE
+//delete a post
+app.delete("/business/id/:id/post/:postid", function(req, res) {
+
+});
+
+
 
 //COMMENT ROUTERS
+//route: /business/id/:id/post/:postid/ POST
+//create comment
+app.post("/business/id/:id/post/:postid", function(req, res) {
+
+});
+
+//route: /business/id/:id/post/:postid/comments GET
+//get all comments for specific post
+app.post("/business/id/:id/post/:postid/comment", function(req, res) {
+
+});
+
+
+//route: /business/id/:id/post/:id/comment/:commentid DELETE
+//delete a comment
+app.delete("/business/id/:id/post/:postid/comment/:commentid", function(req, res) {
+
+});
 
 
 
