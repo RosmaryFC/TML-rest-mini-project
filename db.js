483e766d4d855d8986062835c21cbf072912922f
Rosmary Fermin <Rosmaryfc@hotmail.com>
2021-03-28 03:21:33 -0400

    add comments, fix db string

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const knexfile = require('./knexfile')
-require('dotenv').config()
 const pg = require('pg-promise')()
+
+//access variables in env file with dotenv dependency
+require('dotenv').config()
 
 const db = pg({
   "host": process.env.DB_HOST,
   "port": 5432,
-  "database": "sleep_tracker",
+  "database": "rest_mini_project",
   "user": process.env.DB_USER,
   "password": process.env.DB_PASS
 })
